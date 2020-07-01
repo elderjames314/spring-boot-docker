a1908c77272fa61325837c3f9b464cbe53a9543a
James Oladimeji <james@aapeliltd.com>
2020-07-01 13:30:16 +0100

    First Installation

---
+++ src/test/java/com/aapeliltd/transaction/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aapeliltd.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
